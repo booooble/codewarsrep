6a6c3cc8faa2b345cfe79bf8bdb3511ed2ed3bd6
Anton <tohalitv@gmail.com>
2016-04-02 19:31:37 +0300

    Added xbonacci method. Changed project structure.

---
+++ codewars/src/StringHelper.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class StringHelper {
+
+    /*
+    Returns abbreviation of the String input in format: 1st letter + amount of removed letters + last letter.
+    Example: "abbreviation-satisfaction yabadabadoo babe" --> "a10n-s10n y9o b2e"
+    */
+    public String abbreviate(String string) {
+        StringBuilder sb = new StringBuilder();
+        if(string != null && string != "") {
+            char[] ch = string.replaceAll("[A-Za-z]+", "").toCharArray();
+            String[] str = string.split("[ -]+");
+            for(int i = 0; i < str.length; i++) {
+                String x = str[i];
+                if (x.length() > 1) {
+                    if (string.length() < 3) {
+                        sb.append(x);
+                    }
+                    else {
+                        long length = x.length() - 2;
+                        sb.append(x.substring(0, 1) + length + x.substring(x.length() - 1));
+                    }
+                    if(ch.length != 0 && i < ch.length) {
+                        sb.append(String.valueOf(ch[i]));
+                    }
+                }
+            }
+        }
+        else {
+            return "String is invalid.";
+        }
+        return sb.toString();
+    }
+
+    /*
+    Modifies and returns String where each word stars from the capital letter.
+    Example: "most trees are blue" -> "Most Trees Are Blue"
+    */
+    public String toJadenCase(String phrase) {
+        StringBuilder str = new StringBuilder();
+        if(phrase != null) {
+            String[] mas = phrase.split(" ");
+            for(String x : mas) {
+                str.append(x.substring(0, 1).toUpperCase() + x.substring(1));
+                if(Arrays.asList(mas).lastIndexOf(x) != mas.length - 1) {
+                    str.append(" ");
+                }
+            }
+        }
+        else if(phrase != null || phrase == "") {
+            return null;
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new StringHelper().abbreviate("abbreviation-satisfaction yabadabadoo babe"));
+        System.out.println(new StringHelper().toJadenCase("most trees are blue"));
+    }
+}
