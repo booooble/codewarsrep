1e94d65ded926c4fe51a81879296c578c9b87090
Anton <tohalitv@gmail.com>
2016-04-02 17:51:19 +0300

    Added tribonacci method

--- codewars/src/Abbreviator.java
+++ codewars/src/Abbreviator.java
@@ -3,6 +3,10 @@ Returns abbreviation of the String input in format: 1st letter + amount of remov
 Example: "abbreviation-satisfaction yabadabadoo babe" -> "a10n-s10n y9o b2e"
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Abbreviator {
 
     public String abbreviate(String string) {
@@ -32,7 +36,32 @@ public class Abbreviator {
         return sb.toString();
     }
 
+    /*
+    Returns double[] of n numbers of the tribonacci sequence.
+    Example: {1.0, 1.0, 1.0}, n = 10 -> [1.0, 1.0, 1.0, 3.0, 5.0, 9.0, 17.0, 31.0, 57.0, 105.0]
+    */
+    public double[] tribonacci(double[] s, int n) {
+        double[] result;
+        int count = 0;
+        if(s.length == 0 || s == null || n < 3) {
+            return new double[0];
+        }
+        if(s.length >= 3) {
+            result = Arrays.copyOf(s, n);
+            while(count < n - s.length) {
+                int inputIndex = s.length + count;
+                result[s.length + count] = result[inputIndex - 1] + result[inputIndex - 2] + result[inputIndex - 3];
+                count++;
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Input is incorrect");
+        }
+        return result;
+    }
+
     public static  void main(String[] args){
-        System.out.println(new Abbreviator().abbreviate("abbreviation-satisfaction yabadabadoo babe"));
+//        System.out.println(new Abbreviator().abbreviate("abbreviation-satisfaction yabadabadoo babe"));
+        System.out.println(new Abbreviator().tribonacci(new double[]{1.0,1.0,1.0}, 10));
     }
 }
