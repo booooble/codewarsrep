6466bd6351fbee8fef055b503fca00a43e2f0a6d
Anton Litvinenko <alitvinenko@connecture.com>
2016-04-03 15:11:18 +0300

    tests for StringHelper class

--- codewars/src/junittests/StringHelperTest.java
+++ codewars/src/junittests/StringHelperTest.java
@@ -1,5 +1,6 @@
 package junittests;
 
+import javacode.StringHelper;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -7,11 +8,16 @@ public class StringHelperTest {
 
     @Test
     public void testAbbreviate() throws Exception {
-
+        assertEquals("i18n", StringHelper.abbreviate("internationalization"));
+        assertNull("Must return null when the arg is null", StringHelper.abbreviate(null));
+        assertNull("Must return null when the arg is null", StringHelper.abbreviate(""));
     }
 
     @Test
     public void testToJadenCase() throws Exception {
+            assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", StringHelper.toJadenCase("most trees are blue"), "Most Trees Are Blue");
+            assertNull("Must return null when the arg is null", StringHelper.toJadenCase(null));
+            assertNull("Must return null when the arg is null", StringHelper.toJadenCase(""));
 
     }
 }
